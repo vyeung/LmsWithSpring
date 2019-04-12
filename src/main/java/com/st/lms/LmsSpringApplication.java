679c61fef60aa618c280e37eba57b1d7ec098e83
Tonny Huang <tonny.huang0724@gmail.com>
2019-04-12 16:54:41 -0400

    TH Admin Author Methods

--- src/main/java/com/st/lms/LmsSpringApplication.java
+++ src/main/java/com/st/lms/LmsSpringApplication.java
@@ -1,4 +1,4 @@
-package com.st.lms.app;
+package com.st.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/st/lms/controller/AdminController.java
+++ src/main/java/com/st/lms/controller/AdminController.java
@@ -2,7 +2,10 @@ package com.st.lms.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.st.lms.models.Publisher;
+import com.st.lms.models.*;
 import com.st.lms.service.AdminService;
 
 @RestController
@@ -45,5 +48,57 @@ public class AdminController {
 		adminService.deletePublisher(id);
 	}
 	
+	//TODO: Author
+	@GetMapping(path="/author/{authorId}", produces= {"application/json"})
+    public ResponseEntity<com.st.lms.models.Author> getAuthor(@PathVariable("authorId") int authorId){
+		com.st.lms.models.Author author;
+        ResponseEntity<com.st.lms.models.Author> response;
+        
+        author = adminService.getAuthor(authorId);
+        if(author == null)
+            response = new ResponseEntity<Author>(author, HttpStatus.BAD_REQUEST);
+        else {
+            response = new ResponseEntity<Author>(author, HttpStatus.OK);
+        }
+        return response;
+		
+	}
 	
+	@PostMapping(path="/author/")
+	public ResponseEntity<Author> postAuthor(@RequestBody Author author){
+        ResponseEntity<Author> response;
+        if(adminService.getAuthor(author.getAuthorId()) != null)
+            return new ResponseEntity<Author>(author, HttpStatus.CONFLICT);
+        return new ResponseEntity<Author>(author, HttpStatus.CREATED);
+	}
+	
+	//TODO: check if {authorId} works if I don't have it as a path variable
+	@PutMapping(path="/author/{authorId}")
+	public ResponseEntity<Author> Author(@PathVariable("authorId") int authorId){
+		Author author;
+        ResponseEntity<Author> response;
+        author = adminService.getAuthor(authorId);
+        
+        if(author == null)
+            response = new ResponseEntity<Author>(author, HttpStatus.BAD_REQUEST);
+        else {
+            response = new ResponseEntity<Author>(author, HttpStatus.OK);
+        }
+        return response;
+	}
+	
+	@DeleteMapping(path="/author/{authorId}")
+	public ResponseEntity<Author> deleteAuthor(@PathVariable("authorId") int authorId){
+		Author author;
+        ResponseEntity<Author> response;
+        
+        author = adminService.getAuthor(authorId);
+        if(author == null)
+            response = new ResponseEntity<Author>(author, HttpStatus.BAD_REQUEST);
+        else {
+            response = new ResponseEntity<Author>(author, HttpStatus.OK);
+        }
+        return response;
+		
+	}
 }
