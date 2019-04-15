f5be4f8366fd815dfc964085cdce6949d567ab35
vyeung <vl.yeung@yahoo.com>
2019-04-14 21:46:23 -0400

    add GlobalExceptionHandler

---
+++ src/main/java/com/st/lms/exception/GlobalExceptionHandler.java
@@ -0,0 +1,62 @@
+package com.st.lms.exception;
+
+import java.sql.SQLException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler({NotFoundException.class, BadRequestException.class, AlreadyExistsException.class})
+	public ResponseEntity<ApiError> handleRegularExceptions(Exception ex, WebRequest request) {
+		
+		if(ex instanceof NotFoundException) {
+			ApiError apiE = new ApiError(404, "NOT FOUND", ex.getMessage());
+			return new ResponseEntity<>(apiE, HttpStatus.NOT_FOUND);
+		}
+		else if(ex instanceof BadRequestException) {
+			ApiError apiE = new ApiError(400, "BAD REQUEST", ex.getMessage());
+			return new ResponseEntity<>(apiE, HttpStatus.BAD_REQUEST);
+		}
+		else if(ex instanceof AlreadyExistsException) {
+			ApiError apiE = new ApiError(409, "CONFLICT", ex.getMessage());
+			return new ResponseEntity<>(apiE, HttpStatus.CONFLICT);
+		}
+		else {
+			ApiError apiE = new ApiError(500, "INTERNAL_SERVER_ERROR", ex.getMessage());
+			return new ResponseEntity<>(apiE, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<ApiError> handleBadParameterTypeInURL(Exception ex) {
+		ApiError apiE = new ApiError(400, "BAD REQUEST", "A parameter type in URL doesn't match the expected type");
+		return new ResponseEntity<>(apiE, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<ApiError> handleBadDate(Exception ex) {
+		ApiError apiE = new ApiError(400, "BAD REQUEST", "Date in request body doesn't follow yyyy-MM-dd format");
+		return new ResponseEntity<>(apiE, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ResponseEntity<ApiError> handleEntireApi404(Exception ex) {
+		ApiError apiE = new ApiError(404, "NOT FOUND", "The entered path doesn't match any known paths for this API");
+		return new ResponseEntity<>(apiE, HttpStatus.NOT_FOUND);
+	}
+	
+	
+//	@ExceptionHandler(SQLException.class)
+//	public ResponseEntity<ApiError> handleDBProblem(Exception ex) {
+//		ApiError apiE = new ApiError(500, "INTERNAL_SERVER_ERROR", "Something went wrong on our end");
+//		return new ResponseEntity<>(apiE, HttpStatus.INTERNAL_SERVER_ERROR);
+//	}
+}
