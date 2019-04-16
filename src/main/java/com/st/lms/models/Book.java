840c48033baaea61b1f3ca618eb29fe3545373d2
vyeung <vl.yeung@yahoo.com>
2019-04-16 14:33:39 -0400

    convert Book model to hibernate

--- src/main/java/com/st/lms/models/Book.java
+++ src/main/java/com/st/lms/models/Book.java
@@ -1,10 +1,32 @@
 package com.st.lms.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Entity
+@Table(name = "tbl_book")
 public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int bookId;
+	
+	@Column(name = "title")
 	private String title;
+	
+	@Column(name = "authId")
 	private int authorId;
+	
+	@Column(name = "pubId")
 	private int pubId;
+	
 	
 	public Book() {}
 	
