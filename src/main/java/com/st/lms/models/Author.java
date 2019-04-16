90666e95e1e1dd5786ed73633b159a9a3f7c0de7
Tonny Huang <tonny.huang0724@gmail.com>
2019-04-16 14:30:13 -0400

    Author

--- src/main/java/com/st/lms/models/Author.java
+++ src/main/java/com/st/lms/models/Author.java
@@ -1,7 +1,24 @@
 package com.st.lms.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Entity
+@Table(name = "tbl_author")
 public class Author {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int authorId;
+	
+	@Column(name="authorName")
 	private String authorName;
 	
 	public Author() {}
