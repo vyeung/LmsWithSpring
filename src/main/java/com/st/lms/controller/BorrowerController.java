9663702124ee63a0766ddb8ccb21cc888f93d54a
vyeung <vl.yeung@yahoo.com>
2019-04-18 14:22:54 -0400

    add /borrower mapping to BorrowerController

--- src/main/java/com/st/lms/controller/BorrowerController.java
+++ src/main/java/com/st/lms/controller/BorrowerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.st.lms.service.BorrowerService;
@@ -17,6 +18,7 @@ import com.st.lms.dto.*;
 import com.st.lms.exception.*;
 
 @RestController
+@RequestMapping("/borrower")
 public class BorrowerController {
 
 	@Autowired
