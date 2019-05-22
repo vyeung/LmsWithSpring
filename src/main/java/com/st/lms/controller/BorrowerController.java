51ac847d6e7c595c842b95d86dc629791ce7cb05
vyeung <vl.yeung@yahoo.com>
2019-05-21 22:17:23 -0400

    remove some warnings

--- src/main/java/com/st/lms/controller/BorrowerController.java
+++ src/main/java/com/st/lms/controller/BorrowerController.java
@@ -9,15 +9,16 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.st.lms.dto.BkLoansBkAuthDTO;
+import com.st.lms.dto.BkLoansBranchDTO;
+import com.st.lms.exception.BadRequestException;
+import com.st.lms.exception.NotFoundException;
+import com.st.lms.models.BookLoans;
 import com.st.lms.service.BorrowerService;
-import com.st.lms.models.*;
-import com.st.lms.dto.*;
-import com.st.lms.exception.*;
 
 @RestController
 @RequestMapping("/borrower")
@@ -54,26 +55,26 @@ public class BorrowerController {
 	}
 	
 	//borrow a book
-		@PostMapping("/cardNo/{cardNo}/checkout")
-		public ResponseEntity<BookLoans> borrowBook(@PathVariable int cardNo, @RequestBody BookLoans loan) throws NotFoundException, BadRequestException{
-			if(!borrowerService.cardNoExists(cardNo)) {
-				throw new NotFoundException("Login", "borrower", cardNo);
-			}
-			else if(!borrowerService.branchExists(loan.getBranchId())) {
-				throw new NotFoundException("Check out", "library branch", loan.getBranchId());
-			}
-			else if(!borrowerService.loanExists(cardNo, loan.getBranchId(), loan.getBookId())) {
-				throw new NotFoundException("Check out", "book loan", loan.getBookId());
-			}
-			int noOfCopies = borrowerService.getNoOfCopies(loan.getBookId(), loan.getBranchId());
-			if(noOfCopies < 1) {
-				throw new BadRequestException("Check out failed. That library does not have any copies available.");
-			}
-			else {
-				borrowerService.checkOutBook(loan.getBookId(), loan.getBranchId(), cardNo, noOfCopies);
-				return new ResponseEntity<>(HttpStatus.OK);
-			}
+	@PostMapping("/cardNo/{cardNo}/checkout")
+	public ResponseEntity<BookLoans> borrowBook(@PathVariable int cardNo, @RequestBody BookLoans loan) throws NotFoundException, BadRequestException{
+		if(!borrowerService.cardNoExists(cardNo)) {
+			throw new NotFoundException("Login", "borrower", cardNo);
 		}
+		else if(!borrowerService.branchExists(loan.getBranchId())) {
+			throw new NotFoundException("Check out", "library branch", loan.getBranchId());
+		}
+		else if(!borrowerService.loanExists(cardNo, loan.getBranchId(), loan.getBookId())) {
+			throw new NotFoundException("Check out", "book loan", loan.getBookId());
+		}
+		int noOfCopies = borrowerService.getNoOfCopies(loan.getBookId(), loan.getBranchId());
+		if(noOfCopies < 1) {
+			throw new BadRequestException("Check out failed. That library does not have any copies available.");
+		}
+		else {
+			borrowerService.checkOutBook(loan.getBookId(), loan.getBranchId(), cardNo, noOfCopies);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+	}
 	
 	//return a book
 	@DeleteMapping("/cardNo/{cardNo}/libraries/{branchId}/books/{bookId}")
--- src/main/java/com/st/lms/service/BorrowerService.java
+++ src/main/java/com/st/lms/service/BorrowerService.java
@@ -2,7 +2,6 @@ package com.st.lms.service;
 
 import java.sql.Date;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
