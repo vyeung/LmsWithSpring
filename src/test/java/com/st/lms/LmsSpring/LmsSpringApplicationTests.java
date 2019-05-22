5278232d15730962f104afc57ace5be33f2e154c
vyeung <vl.yeung@yahoo.com>
2019-05-21 22:16:28 -0400

    comment out test file

--- src/test/java/com/st/lms/LmsSpring/LmsSpringApplicationTests.java
+++ src/test/java/com/st/lms/LmsSpring/LmsSpringApplicationTests.java
@@ -1,16 +1,16 @@
-package com.st.lms.LmsSpring;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class LmsSpringApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package com.st.lms.LmsSpring;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class LmsSpringApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
